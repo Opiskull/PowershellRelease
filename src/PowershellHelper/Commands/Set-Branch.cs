952314dc8c95fed1b895cb506409116bf1b837c9
Opiskull <Christian.Onuk@gmx.net>
2015-12-18 21:28:17 +0100

    feat(set-branch): add set-branch command for checkouts

---
+++ src/PowershellHelper/Commands/Set-Branch.cs
@@ -0,0 +1,23 @@
+﻿using System.Management.Automation;
+using PowershellHelper.Services;
+
+namespace PowershellHelper.Commands
+{
+    [Cmdlet(VerbsCommon.Set, "Branch",
+        DefaultParameterSetName = "SetBranch")]
+    public class SetBranch : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string RepositoryPath { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public string Branch { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var githelper = new GitRepositoryHelper(RepositoryPath);
+            WriteVerbose($"Check out {Branch}");
+            githelper.CheckOut(Branch);
+        }
+    }
+}
